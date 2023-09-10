0ceb4b43a356ddab289fda85c1a138f12aec8ba5
Kyandroo <kyandevo@student.arteveldehs.be>
2023-09-10 17:40:56 +0200

    Remove files from gh-pages branch

--- js/app.js
+++
@@ -1,9 +0,0 @@
-import hamburgerMenu from "./adds/hamburger.js";
-import digitalTimer from "./adds/contact-timer.js";
-
-const $clock = document.getElementById("contact-clock");
-
-hamburgerMenu();
-if ($clock) {
-  digitalTimer();
-}
