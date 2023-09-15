5293ab2b9512c42842b5a77dfb3574d2167f6b4b
Kyandroo <kyandevo@student.arteveldehs.be>
2023-09-15 14:30:35 +0200

    Added pathPrefix back to output folder

--- .eleventy.js
+++ .eleventy.js
@@ -23,7 +23,7 @@ module.exports = (eleventyConfig) => {
       includes: "../_partials",
       layouts: "../_layouts",
       data: "../_data",
-      // pathPrefix: "/output/",
+      pathPrefix: "/output/",
     },
   };
 };
